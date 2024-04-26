861869f22e3c592c25a7a6647d3d93ade56c91c2
BKSVC <burak.savci2000@gmail.com>
2024-04-27 01:48:28 +0300

    brute force saldırılarına karşı anti sistem geliştirildi

---
+++ spamhunter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+memorychecker(){
+        owned_ip_address=($(iptables -L INPUT -v | awk '{if ($0 ~ /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/) {match($0, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); print substr($0, RSTART, RLENGTH)}}'))
+}
+dump_spam_users(){
+        dump_spams=($(grep "SASL LOGIN authentication failed" /var/log/mail.log | awk '{if ($0 ~ /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/) {match($0, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); print substr($0, RSTART, RLENGTH)}}'))
+}
+hunter(){
+memorychecker
+dump_spam_users
+for hunt in "${dump_spams[@]}"
+do
+        for listed in "${owned_ip_address[@]}"
+        do
+                if [[ "$hunt" == "$listed" ]];then
+                        echo "önceki kayıt tespit edildi: $listed"
+                else
+                        echo "yeni kayıt bulundu : $hunt"
+                fi
+        done
+done
+}
+hunter
