67eae52b51d0375b695711bfab4dd17792d330eb
BKSVC <burak.savci2000@gmail.com>
2024-04-29 18:54:25 +0300

    latest update

--- spamhunter.sh
+++ spamhunter.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 memorychecker(){
-        owned_ip_address=($(iptables -L INPUT -v | awk '{if ($0 ~ /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/) {match($0, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); print substr($0, RSTART, RLENGTH)}}'))
+        owned_ip_address=($(/usr/sbin/iptables -L INPUT -v | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'| sort | uniq ))
 }
 dump_spam_users(){
-        dump_spams=($(grep "SASL LOGIN authentication failed" /var/log/mail.log | awk '{if ($0 ~ /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/) {match($0, /[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); print substr($0, RSTART, RLENGTH)}}'))
+        dump_spams=($(grep "SASL LOGIN authentication failed" /var/log/mail.log | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'| sort | uniq ))
+
 }
-hunter(){
 memorychecker
 dump_spam_users
-for hunt in "${dump_spams[@]}"
-do
-        for listed in "${owned_ip_address[@]}"
-        do
-                if [[ "$hunt" == "$listed" ]];then
-                        echo "önceki kayıt tespit edildi: $listed" >> /home/burak/shlogs/logs
-                else
-                        echo "yeni kayıt bulundu : $hunt" >> /home/burak/shlogs/logs
-                        iptables -A INPUT -s $hunt -j DROP
-
-                fi
-        done
-done
-}
-hunter
